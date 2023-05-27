22775e71bcfe514369904738f06433def596bb62
kekefarin <keisha.adhaira@gmail.com>
2023-05-27 22:45:34 +0700

    first commit

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer ">
+    <div class="container ">
+        <span class="text-muted ">Keisha Farin Adhaira.</span>
+    </div>
+</footer>
---
+++ app/Views/layouts/header.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-5 ">Welcome to Web Keke </h1>
+    <p class="lead mb-5">Which is more difficult, coding or counting? Not both of them, the difficult one is sharing your knowledge with people without asking for the payment. <br/> <small>- Keke</small></p>
+    <p>Kunjungi blog belajar bareng Keke dengan akses gratis <a href="https://keke.xyz ">disini</a>.</p>
+</div>
